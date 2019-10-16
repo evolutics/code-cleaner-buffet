603e9bd9011d7e7c20a6863cb272652f44c460ea
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-10-16 08:00:16 +0200

    Check with Git for conflict markers and whitespace errors

--- ci/check.sh
+++ ci/check.sh
@@ -4,12 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+check_with_git() {
+  git diff --check HEAD^
+}
+
 check_with_hadolint() {
   git ls-files -z -- '*/Dockerfile' Dockerfile '*.Dockerfile' \
     | xargs -0 hadolint
 }
 
 main() {
+  check_with_git
   check_with_hadolint
 }
 
