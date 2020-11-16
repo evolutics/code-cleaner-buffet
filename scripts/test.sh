08329aaf691be2b7b4e5fc79d72ab29f70e74af1
Benjamin Fischer <benjamin.fischer@evolutics.info>
2020-11-16 22:35:41 +0100

    Simplify changing folder in Bash

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,14 +7,11 @@ set -o pipefail
 main() {
   local -r script_folder="$(dirname "$(readlink --canonicalize "$0")")"
   local -r project_folder="$(dirname "${script_folder}")"
-
-  pushd "${project_folder}"
+  cd "${project_folder}"
 
   docker run --entrypoint sh --rm --volume "$(pwd)":/workdir \
     evolutics/travel-kit:0.6.0 -c \
     'git ls-files -z | xargs -0 travel-kit check --'
-
-  popd
 }
 
 main "$@"
